f4dc42ac3e8ed38eb2278fb3b1bb3c22bd70b075
mehakghous <mehakghous271204@gmail.com>
2021-10-16 22:26:20 +0500

    component bind with state

--- src/header.js
+++ src/header.js
@@ -1,15 +1,24 @@
+import React from 'react'
 import './header.css'
-function Header(){
-    return(
-        <div className = "head-main">
-            <h1 className = "title">Home page</h1>
-            <div className = 'links'>
-                 <span>Home</span>
-                 <span>About</span>
-                 <span>Services</span>
-                 <span>Contact</span>
+class Header extends React.Component{
+
+    state = {
+        appName: "Practice home-page"
+    }
+    render(){
+        return (
+            <div className="head-main">
+                <h1 className="title">{this.state.appName}</h1>
+                <div className='links'>
+                    <span>Home</span>
+                    <span>About</span>
+                    <span>Services</span>
+                    <span>Contact</span>
+                </div>
             </div>
-        </div>
-    )
+        )
+
+    }
+
 }
 export default Header
